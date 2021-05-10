f7a761b47193ba8d65627774ae43bd4a9a98df3f
w.hunt <wfh2d88@gmail.com>
2021-05-10 04:14:36 -0500

    image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
     <Container id="about" className="about">
       <Row>
         <Col md={12}>
-        <img src='./assets/images/me.jpg' alt="alt"/>
+        <img href='https://imgur.com/tEgInoB' alt="alt"/>
         </Col>
         <Col md={12}>
         <p>I am a web developer from Nashville, Tn. Here comes some Lorem ipsum 
